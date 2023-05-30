9577b9130db84e9070634c4a3babebab95c7c0c5
Thuyngan <lanthuyngan@gmail.com>
2023-05-30 16:39:33 +0700

    ASM lan 2

--- ASM1/src/app/Interface/Products.ts
+++
@@ -1,7 +0,0 @@
-export interface Iproduct {
-  id?: Number,
-  name: String,
-  price: Number,
-  desc: String,
-  images?: String
-}
--- asm/src/app/Interface/Products.ts
+++
@@ -1,6 +0,0 @@
-export interface IProduct {
-  id?: Number,
-  name: String,
-  price: Number
-
-}
